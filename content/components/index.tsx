0041a5f78039fb35f455001c5a96f6de877c71f0
Ming Ying <ming@whist.com>
2022-12-02 21:24:47 -0500

    highlighting works

--- content/components/index.tsx
+++ content/components/index.tsx
@@ -2,62 +2,58 @@ import React, { useEffect, useState, useRef } from "react"
 import Save from "./save"
 import Success from "./success"
 
-const specialKey = "Alt"
+const specialKey = "c"
 const enterKey = "Enter"
 
 export default () => {
-  const [keyPressed, _setKeyPressed] = useState(false)
-  const [textSelected, _setTextSelected] = useState("")
   const [showSuccess, setShowSuccess] = useState(false)
+  const [showPopup, setShowPopup] = useState(false)
+  const [selectedNode, setSelectedNode] = useState<
+    ParentNode | undefined | null
+  >(undefined)
 
-  const shiftPressedRef = useRef(keyPressed)
-  const setShiftPressed = (data) => {
-    shiftPressedRef.current = data
-    _setKeyPressed(data)
+  const onSave = (text: string) => {
+    // Save it
+    const parentNode = selectedNode?.parentNode
+    let clonedNode = selectedNode?.cloneNode(true) as any
+
+    if (
+      clonedNode !== undefined &&
+      selectedNode !== undefined &&
+      selectedNode !== null
+    ) {
+      clonedNode.innerHTML = `<span class="bg-[#ACCEF7] bg-opacity-50 underline decoration-indigo-700">${selectedNode.innerHTML}</span>`
+      parentNode?.replaceChild(clonedNode, selectedNode)
+    }
+
+    setShowPopup(false)
+    setShowSuccess(true)
+    setSelectedNode(undefined)
+
+    setTimeout(() => {
+      setShowSuccess(false)
+    }, 3000)
   }
 
-  const textedSelectedRef = useRef(textSelected)
-  const setTextSelected = (data) => {
-    textedSelectedRef.current = data
-    _setTextSelected(data)
+  const onCancel = () => {
+    setShowPopup(false)
+    setSelectedNode(undefined)
   }
 
   useEffect(() => {
-    document.addEventListener("selectionchange", () => {
-      if (shiftPressedRef.current) {
-        setTextSelected(window.getSelection()?.toString() ?? "")
-      } else {
-        setTextSelected("")
-      }
-    })
-
     window.addEventListener("keydown", (e) => {
-      if (e.key === specialKey) setShiftPressed(true)
-    })
+      if (e.key !== specialKey) return
+      if (e.metaKey) return
+      if (e.ctrlKey) return
+      if ((window.getSelection()?.toString() ?? "") === "") return
 
-    window.addEventListener("keydown", (e) => {
-      if (e.key === enterKey && textedSelectedRef.current !== "") {
-        // Save it
-        const selectedNode = window.getSelection()?.getRangeAt(0).startContainer
-          .parentNode as any
-        const parentNode = selectedNode?.parentNode
-        let template = document.createElement("template")
-        template.innerHTML = `<span class="bg-indigo-200 underline decoration-indigo-700">${selectedNode?.innerHTML}</span>`
-        parentNode.replaceChild(template.content.firstChild, selectedNode)
-
-        setTextSelected("")
-        setShowSuccess(true)
-        setTimeout(() => {
-          setShowSuccess(false)
-        }, 3000)
-      }
-    })
-
-    window.addEventListener("keyup", (e) => {
-      if (e.key === specialKey) setShiftPressed(false)
+      setSelectedNode(
+        window.getSelection()?.getRangeAt(0).startContainer.parentNode
+      )
+      setShowPopup(true)
     })
   }, [])
 
   if (showSuccess) return <Success />
-  return <Save open={textSelected !== ""} />
+  return <Save open={showPopup} onSave={onSave} onCancel={onCancel} />
 }
--- content/components/save.tsx
+++ content/components/save.tsx
@@ -1,7 +1,11 @@
 import React, { Fragment } from "react"
 import { Transition } from "@headlessui/react"
 
-export default (props: { open: boolean }) => {
+export default (props: {
+  open: boolean
+  onSave: (text: string) => void
+  onCancel: () => void
+}) => {
   return (
     <Transition
       as={Fragment}
@@ -13,29 +17,46 @@ export default (props: { open: boolean }) => {
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
-      <div className="fixed z-[2147483647] bottom-3 left-3 rounded-xl text-gray-100 shadow-2xl bg-indigo-200">
-        <button
-          type="button"
-          className="flex space-x-6 items-center rounded-md bg-indigo-200 px-6 py-3 text-base font-medium text-indigo-700 focus:outline-none"
-        >
-          <div>Save</div>
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-4 h-4 text-indigo-700"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-              />
-            </svg>
+      <div className="fixed z-[2147483647] bottom-3 left-3 rounded text-gray-100 shadow-2xl bg-white w-full max-w-sm">
+        <form action="#" className="relative">
+          <div className="overflow-hidden">
+            <label htmlFor="comment" className="sr-only">
+              Add your comment
+            </label>
+            <textarea
+              autoFocus
+              rows={3}
+              name="comment"
+              id="comment"
+              className="block w-full resize-none border-none outline-none p-3 sm:text-sm text-gray-900"
+              placeholder="Add your comment..."
+              defaultValue={""}
+            />
+
+            <div className="py-2" aria-hidden="true">
+              <div className="py-px">
+                <div className="h-9" />
+              </div>
+            </div>
           </div>
-        </button>
+
+          <div className="absolute inset-x-0 bottom-0 py-2 pl-3 pr-2 flex space-x-2 justify-right">
+            <button
+              onClick={() => props.onCancel()}
+              type="submit"
+              className="inline-flex items-center rounded-md border border-red-200 px-4 py-2 text-sm font-medium text-red-600 shadow-sm focus:outline-none"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={() => props.onSave("")}
+              type="submit"
+              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none"
+            >
+              Save
+            </button>
+          </div>
+        </form>
       </div>
     </Transition>
   )
